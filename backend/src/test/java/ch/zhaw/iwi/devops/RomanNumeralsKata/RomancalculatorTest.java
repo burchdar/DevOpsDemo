523d0023081ae54dd8343b1fb9ff758180ceaa7d
Dario Burch <burchdar@students.zhaw.ch>
2023-06-04 02:09:26 +0200

    Commit of changed Tests 1-4 due to new program code

--- backend/src/test/java/ch/zhaw/iwi/devops/RomanNumeralsKata/RomancalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/RomanNumeralsKata/RomancalculatorTest.java
@@ -10,35 +10,34 @@ public class RomancalculatorTest {
     @Test
     public void test1() {
         Romancalculator number = new Romancalculator();
-        assertThat("X").isEqualTo(number.convert(10));         
+        assertThat("X").isEqualTo(number.convert("10"));         
     }
 
     @Test
     public void test2() {
         Romancalculator number = new Romancalculator();
-        assertThat("IV").isEqualTo(number.convert(4));         
+        assertThat("IV").isEqualTo(number.convert("4"));         
     }
 
     @Test
     public void test3() {
         Romancalculator number = new Romancalculator();
-        assertEquals("I", number.convert(1));
-        assertEquals("II", number.convert(2));
-        assertEquals("III", number.convert(3));
-        assertEquals("IV", number.convert(4));
-        assertEquals("V", number.convert(5));
-        assertEquals("VI", number.convert(6));
-        assertEquals("VII", number.convert(7));
-        assertEquals("VIII", number.convert(8));
-        assertEquals("IX", number.convert(9));
-        assertEquals("X", number.convert(10));       
+        assertEquals("I", number.convert("1"));
+        assertEquals("II", number.convert("2"));
+        assertEquals("III", number.convert("3"));
+        assertEquals("IV", number.convert("4"));
+        assertEquals("V", number.convert("5"));
+        assertEquals("VI", number.convert("6"));
+        assertEquals("VII", number.convert("7"));
+        assertEquals("VIII", number.convert("8"));
+        assertEquals("IX", number.convert("9"));
+        assertEquals("X", number.convert("10"));       
     }
     @Test
     public void test4() {
         Romancalculator number = new Romancalculator();
-        assertEquals("Bitte nur Zahlen von 1-10 eingeben", number.convert(11));
-        assertEquals("Bitte nur Zahlen von 1-10 eingeben", number.convert(0));
-        assertEquals("Bitte nur Zahlen von 1-10 eingeben", number.convert(-1));      
+        assertEquals("Bitte nur Zahlen von 1-1000 eingeben", number.convert("0"));
+        assertEquals("Bitte nur Zahlen von 1-1000 eingeben", number.convert("-1"));      
     }
 
     @Test
